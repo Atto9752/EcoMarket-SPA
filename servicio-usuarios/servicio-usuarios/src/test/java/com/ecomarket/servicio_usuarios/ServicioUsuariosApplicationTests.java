bb78125ab5c185501cc5dcbd8eb09f41a0bab474
Atto9752 <anto.faundes@duocuc.cl>
2025-05-24 15:37:06 -0400

    nuevo proyecto ecomarket

---
+++ servicio-usuarios/servicio-usuarios/src/test/java/com/ecomarket/servicio_usuarios/ServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.servicio_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
