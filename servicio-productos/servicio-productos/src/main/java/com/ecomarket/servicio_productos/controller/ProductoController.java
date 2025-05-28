27895aca2dd0ae1643f144f89b0737aab310b40a
Atto9752 <anto.faundes@duocuc.cl>
2025-05-27 21:19:15 -0400

    cambios en controlador y modelo (faltan cosas aun)

--- servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/controller/ProductoController.java
+++ servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/controller/ProductoController.java
@@ -1,5 +1,38 @@
 package com.ecomarket.servicio_productos.controller;
 
+import java.util.List;
+import com.ecomarket.servicio_productos.model.Producto;
+import com.ecomarket.servicio_productos.service.ProductoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/productos")
+
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listaProductos() {
+        return productoService.listaProductos();
+    }
+
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto) {
+        return productoService.saveProducto(producto);
+    }
+
+    @GetMapping("{id}")
+    public Producto buscarProducto(@PathVariable int id) {
+        return productoService.getProductoId(id);
+    }
+
 
 }
--- servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/model/Producto.java
+++ servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/model/Producto.java
@@ -1,5 +1,20 @@
 package com.ecomarket.servicio_productos.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Producto {
 
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int precio;
+    private int stock;
+    private String categoria;
+    
 }
