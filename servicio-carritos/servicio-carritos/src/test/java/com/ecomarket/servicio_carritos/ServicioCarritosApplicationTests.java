411863154266790526efaad8dc370273d06e8608
Atto9752 <anto.faundes@duocuc.cl>
2025-05-29 15:18:54 -0400

    cambios a microservicios

---
+++ servicio-carritos/servicio-carritos/src/test/java/com/ecomarket/servicio_carritos/ServicioCarritosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.servicio_carritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCarritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ servicio-tiendas/servicio-tiendas/src/test/java/com/ecomarket/servicio_tiendas/ServicioTiendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.servicio_tiendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioTiendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
