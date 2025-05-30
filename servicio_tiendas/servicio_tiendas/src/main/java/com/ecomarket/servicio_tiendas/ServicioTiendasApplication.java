5d2e42f2a5f7fd579263dab5df020df655b1a7d1
Pablo6OO <paba.diaz@duocuc.cl>
2025-05-30 13:30:10 -0400

    Cambio a la carpeta en si

---
+++ servicio_tiendas/servicio_tiendas/src/main/java/com/ecomarket/servicio_tiendas/ServicioTiendasApplication.java
@@ -0,0 +1,13 @@
+package com.ecomarket.servicio_tiendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioTiendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioTiendasApplication.class, args);
+	}
+
+}
