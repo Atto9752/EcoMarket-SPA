4629155836a60d822b2cf7eb0e93741991530c1b
Atto9752 <anto.faundes@duocuc.cl>
2025-05-30 15:57:46 -0400

    pequeñas mejoras a productos

--- servicio-carritos/servicio-carritos/src/main/java/com/ecomarket/servicio_carritos/model/Carrito.java
+++ servicio-carritos/servicio-carritos/src/main/java/com/ecomarket/servicio_carritos/model/Carrito.java
@@ -1,5 +1,13 @@
 package com.ecomarket.servicio_carritos.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "carrito")  // Nombre de la tabla en la base de datos
+@Data
+
 public class Carrito {
 
 }
--- servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/model/Producto.java
+++ servicio-productos/servicio-productos/src/main/java/com/ecomarket/servicio_productos/model/Producto.java
@@ -6,14 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;  // No es necesario pq @Data ya lo incluye
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "producto")   // Nombre de la tabla en la base de datos
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor  no es necesario con @Data
 @NoArgsConstructor
 
 public class Producto {
