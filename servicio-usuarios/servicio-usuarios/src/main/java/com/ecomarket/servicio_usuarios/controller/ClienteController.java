74b1a5f58f0695b7d6e8e3e05a92e5305ee3923c
Pablo6OO <paba.diaz@duocuc.cl>
2025-05-30 13:25:09 -0400

    Me equivoque de carpeta

--- servicio-usuarios/servicio-usuarios/src/main/java/com/ecomarket/servicio_usuarios/controller/ClienteController.java
+++ servicio-usuarios/servicio-usuarios/src/main/java/com/ecomarket/servicio_usuarios/controller/ClienteController.java
@@ -1,5 +1,12 @@
 package com.ecomarket.servicio_usuarios.controller;
-import com.ecomarket.servicio_pedidos.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomarket.servicio_carritos.service;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
